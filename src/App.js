73c2b13fb0b0459b7838c0f1ddca30b11c13fda6
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-26 08:38:41 +0300

    install d3 package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { render } from "react-dom";
 
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello React!</h1>
-    </div>
-  );
-};
 
-render(<App />, document.getElementById("root"));
+
+
+
+const drawScatterplot = data => {
+  return
+}
+
+
+
+const api_url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+
+fetch( api_url )
+.then( res => res.json())
+.then( data => {
+  drawScatterplot(data)
+})
